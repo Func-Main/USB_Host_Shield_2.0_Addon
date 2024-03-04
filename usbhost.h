557ea73846737246d027b34a04802d3fac651a00
Daniel Morgan <danmorgz@googlemail.com>
2024-03-04 19:26:50 +0100

    changes for an ESP_DEV board (which may not be the compiler flag... looking for it now

--- usbhost.h
+++ usbhost.h
@@ -118,6 +118,8 @@ typedef SPi< P76, P75, P74, P10 > spi;
 typedef SPi< P16, P18, P17, P10 > spi;
 #elif defined(ESP8266)
 typedef SPi< P14, P13, P12, P15 > spi;
+#elif defined(ESP32_DEV)
+typedef SPi< P6, P7, P15, P12 > spi;
 #elif defined(ESP32)
 typedef SPi< P18, P23, P19, P5 > spi;
 #elif defined(ARDUINO_NRF52840_FEATHER)
